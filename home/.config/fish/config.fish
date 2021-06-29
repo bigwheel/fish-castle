09c3fde51c97f6ba2cfc26fc9d139d07f168a121
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-06-30 07:34:18 +0900

    fisherのインストールを自動化

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    # fisherのインストールを自動化
+    if not test -e $__fish_config_dir/fish_plugins
+        curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    end
+
     # asdfのセットアップだけ自動化した。インストールは自動化されていない
     if test -e ~/.asdf
         source ~/.asdf/asdf.fish
