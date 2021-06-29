dc780b4ae9bbba9072b332b10b9056fa0543ce77
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-06-30 06:55:21 +0900

    asdfの自動セットアップ処理を追加

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # asdfのセットアップだけ自動化した。インストールは自動化されていない
+    if test -e ~/.asdf
+        source ~/.asdf/asdf.fish
+        if not test -e ~/.config/fish/completions/asdf.fish
+            mkdir -p ~/.config/fish/completions
+            ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+        end
+    end
 end
