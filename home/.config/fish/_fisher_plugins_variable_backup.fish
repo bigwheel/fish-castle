7f8e53bf02792e9c4d65e133dfb28f340c198b9e
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-08-27 01:33:38 +0900

    fisherプラグインを追加

--- home/.config/fish/_fisher_plugins_variable_backup.fish
+++ home/.config/fish/_fisher_plugins_variable_backup.fish
@@ -1,1 +1,1 @@
-set --universal --unpath --unexport _fisher_plugins 'jorgebucaran/fisher' '2m/fish-history-merge' 'jethrokuan/z' 'PatrickF1/fzf.fish' 'b4b4r07/enhancd'
+set --universal --unpath --unexport _fisher_plugins 'jorgebucaran/fisher' '2m/fish-history-merge' 'jethrokuan/z' 'PatrickF1/fzf.fish' 'b4b4r07/enhancd' 'bigwheel/fish-gitpath'
